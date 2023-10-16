a5bb83465599a42f503beaaaa28fe7f087b9c025
Allan Felintro <allanpdu@gmail.com>
2023-10-16 16:50:38 -0300

    inicio da implementação de usuário e permissões de acesso

--- src/main/java/com/felintro/leonard/model/autenticacao/Permissao.java
+++ src/main/java/com/felintro/leonard/model/autenticacao/Permissao.java
@@ -1,4 +1,33 @@
-package com.felintro.leonard.model;
+package com.felintro.leonard.model.autenticacao;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "permissao")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Permissao {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "uuid")
+    private UUID uuid;
+
+    @Column(name = "ds_nome_permissao")
+    private String nome;
+
+    public Permissao(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
--- src/main/java/com/felintro/leonard/model/autenticacao/Usuario.java
+++ src/main/java/com/felintro/leonard/model/autenticacao/Usuario.java
@@ -1,5 +1,42 @@
-package com.felintro.leonard.model;
+package com.felintro.leonard.model.autenticacao;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "uuid")
+    private UUID uuid;
+
+
+    @Column(name = "nome_usuario", nullable = false)
+    private String nomeUsuario;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Column(name = "ds_nome", nullable = false)
+    private String nome;
+
+    @ManyToMany
+    private List<Permissao> permissoes;
 
 }
