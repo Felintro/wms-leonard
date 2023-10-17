3504585a34e6a2df76c3cc04ec71ee9ded8cca44
Allan Felintro <allanpdu@gmail.com>
2023-10-16 21:31:37 -0300

    Refatoração nos repositories de produto e usuário

--- src/main/java/com/felintro/leonard/repository/ProdutoRepository.java
+++ src/main/java/com/felintro/leonard/repository/ProdutoRepository.java
@@ -1,6 +1,6 @@
 package com.felintro.leonard.repository;
 
-import com.felintro.leonard.model.Produto;
+import com.felintro.leonard.model.estoque.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
--- src/main/java/com/felintro/leonard/repository/UsuarioRepository.java
+++ src/main/java/com/felintro/leonard/repository/UsuarioRepository.java
@@ -1,10 +1,10 @@
 package com.felintro.leonard.repository;
 
-import com.felintro.leonard.model.Usuario;
+import com.felintro.leonard.model.autenticacao.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
-    
+
 }
