131946cf04f0f0f856fdbeabee935473a15aba92
Allan Felintro <allanpdu@gmail.com>
2023-10-17 19:05:00 -0300

    Criação de entidades para pessoas (usuários) e empresas (clientes e fornecedores)

---
+++ src/main/java/com/felintro/leonard/model/pessoa/Empresa.java
@@ -0,0 +1,50 @@
+package com.felintro.leonard.model.pessoa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empresa")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "nr_telefone", unique = true, nullable = false, length = 11)
+    private String nrTelefone;
+
+    @Column(name = "nr_cnpj", nullable = false, unique = true)
+    private String nrCnpj;
+
+    @Column(name = "razao_social", nullable = false)
+    private String razaoSocial;
+
+    @Column(name = "status")
+    private boolean status;
+
+    public Empresa(String email, String nrTelefone, String nrCnpj, String razaoSocial) {
+        this.email = email;
+        this.nrTelefone = nrTelefone;
+        this.nrCnpj = nrCnpj;
+        this.razaoSocial = razaoSocial;
+    }
+
+    public boolean isAtivo() {
+        return this.status;
+    }
+
+}
