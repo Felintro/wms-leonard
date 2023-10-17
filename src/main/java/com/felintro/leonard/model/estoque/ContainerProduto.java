c3f5e50508aece283836c6f30619c3b47076b9d3
Allan Felintro <allanpdu@gmail.com>
2023-10-16 22:09:51 -0300

    Remoção de imports desnecessários

--- src/main/java/com/felintro/leonard/model/estoque/ContainerProduto.java
+++ src/main/java/com/felintro/leonard/model/estoque/ContainerProduto.java
@@ -1,6 +1,5 @@
 package com.felintro.leonard.model.estoque;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
