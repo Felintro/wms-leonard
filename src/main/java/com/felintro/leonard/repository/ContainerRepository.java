9b90743ded01d15e4c0fab828f2da0bd7a893025
Allan Felintro <allanpdu@gmail.com>
2023-10-17 00:25:36 -0300

    Criação de repositories para Container e Endereço

---
+++ src/main/java/com/felintro/leonard/repository/ContainerRepository.java
@@ -0,0 +1,8 @@
+package com.felintro.leonard.repository;
+
+import com.felintro.leonard.model.estoque.Container;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContainerRepository extends JpaRepository<Container, Long> {
+
+}
