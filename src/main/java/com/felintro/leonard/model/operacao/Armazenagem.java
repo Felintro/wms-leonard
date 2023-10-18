857cddaeaf7bad3148f10699920e7d930e395e1e
Allan Felintro <allanpdu@gmail.com>
2023-10-17 21:48:24 -0300

    Remoção de Armazenagem do banco de dados.

--- src/main/java/com/felintro/leonard/model/operacao/Armazenagem.java
+++ src/main/java/com/felintro/leonard/model/operacao/Armazenagem.java
@@ -1,10 +1,5 @@
 package com.felintro.leonard.model.operacao;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "armazenagem")
 public class Armazenagem extends Operacao {
 
 }
