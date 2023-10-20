9238e6c18087811f8c07933e5733e50602d17093
Allan Felintro <allanpdu@gmail.com>
2023-10-20 16:31:50 -0300

    Correção de anotação das colunas de relacionamento

--- src/main/java/com/felintro/leonard/model/operacao/Movimentacao.java
+++ src/main/java/com/felintro/leonard/model/operacao/Movimentacao.java
@@ -2,11 +2,11 @@ package com.felintro.leonard.model.operacao;
 
 import com.felintro.leonard.model.estoque.Container;
 import com.felintro.leonard.model.estoque.Endereco;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,15 +24,15 @@ public class Movimentacao extends Operacao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "endereco_origem_id", nullable = false)
+    @JoinColumn(name = "endereco_origem_id", nullable = false)
     @OneToOne
     private Endereco enderecoOrigem;
 
-    @Column(name = "endereco_destino_id", nullable = false)
+    @JoinColumn(name = "endereco_destino_id", nullable = false)
     @OneToOne
     private Endereco enderecoDestino;
 
-    @Column(name = "container_id", nullable = false)
+    @JoinColumn(name = "container_id", nullable = false)
     @OneToOne
     private Container container;
 
