0dd1f45893a4ef1c5b53c3f83d79823e7f9c5695
Allan Felintro <allanpdu@gmail.com>
2023-10-20 20:28:46 -0300

    Início da implementação da lógica de negócio da movimentação

--- src/main/java/com/felintro/leonard/controller/operacao/MovimentacaoController.java
+++ src/main/java/com/felintro/leonard/controller/operacao/MovimentacaoController.java
@@ -1,9 +1,20 @@
 package com.felintro.leonard.controller.operacao;
 
+import com.felintro.leonard.service.operacao.MovimentacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author allan
  **/
 
+@Controller
+@RequestMapping("/operacao/movimentacao")
 public class MovimentacaoController {
+
+    @Autowired
+    private MovimentacaoService movimentacaoService;
+
 
 }
--- src/main/java/com/felintro/leonard/service/operacao/MovimentacaoService.java
+++ src/main/java/com/felintro/leonard/service/operacao/MovimentacaoService.java
@@ -1,12 +1,17 @@
 package com.felintro.leonard.service.operacao;
 
+import com.felintro.leonard.repository.MovimentacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * @author allan
  **/
 
-@Service
+@Service("movimentacaoService")
 public class MovimentacaoService {
+
+    @Autowired
+    private MovimentacaoRepository movimentacaoRepository;
 
 }
