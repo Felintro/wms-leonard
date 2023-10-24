1c09360612cdbf8ceb7deb68131da19945e74775
Allan Felintro <allanpdu@gmail.com>
2023-10-23 21:08:01 -0300

    Implementação inicial do cadastro e view de produtos

---
+++ src/main/java/com/felintro/leonard/service/estoque/ProdutoService.java
@@ -0,0 +1,36 @@
+package com.felintro.leonard.service.estoque;
+
+import com.felintro.leonard.dto.ProdutoDTO;
+import com.felintro.leonard.model.estoque.Produto;
+import com.felintro.leonard.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author allan
+ **/
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<ProdutoDTO> listarProdutos() {
+        List<Produto> produtos = produtoRepository.findAll();
+        List<ProdutoDTO> retorno = new ArrayList<>();
+        produtos.forEach(produto -> {
+            retorno.add(new ProdutoDTO(produto));
+        });
+
+        return retorno;
+    }
+
+    public void cadastrarProduto(Produto produto) {
+        produtoRepository.save(produto);
+    }
+
+}
