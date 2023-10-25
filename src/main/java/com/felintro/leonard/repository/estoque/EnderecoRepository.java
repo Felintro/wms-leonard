3d06d0fdf2d02e787b647e2e269175ec5def464e
Allan Felintro <allanpdu@gmail.com>
2023-10-24 23:20:06 -0300

    Implementação de método para busca de endereço pelo número do container

--- src/main/java/com/felintro/leonard/repository/estoque/EnderecoRepository.java
+++ src/main/java/com/felintro/leonard/repository/estoque/EnderecoRepository.java
@@ -2,9 +2,8 @@ package com.felintro.leonard.repository.estoque;
 
 import com.felintro.leonard.model.estoque.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * @author allan
@@ -16,5 +15,7 @@ public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
     boolean existsByNrRuaAndNrPredioAndNrApartamento(int nrRua, int nrPredio, int nrApartamento);
 
+    @Query("SELECT e FROM Endereco e WHERE e.container.nrContainer = ?1")
+    Endereco findByNrContainer(Long nrContainer);
 
 }
