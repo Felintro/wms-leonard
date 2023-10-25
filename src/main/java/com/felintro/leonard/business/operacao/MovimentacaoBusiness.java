ec7b8ec73df1483ee0448177dbefeded0de19f6b
Allan Felintro <allanpdu@gmail.com>
2023-10-24 21:23:07 -0300

    Implementação inicial da operação de movimentação

--- src/main/java/com/felintro/leonard/business/operacao/MovimentacaoBusiness.java
+++ src/main/java/com/felintro/leonard/business/operacao/MovimentacaoBusiness.java
@@ -1,11 +1,11 @@
 package com.felintro.leonard.business.operacao;
 
-import com.felintro.leonard.model.operacao.Movimentacao;
+import com.felintro.leonard.model.estoque.Endereco;
+import com.felintro.leonard.repository.estoque.ContainerRepository;
+import com.felintro.leonard.repository.estoque.EnderecoRepository;
 import com.felintro.leonard.repository.operacao.MovimentacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * @author allan
@@ -17,8 +17,14 @@ public class MovimentacaoBusiness {
     @Autowired
     private MovimentacaoRepository movimentacaoRepository;
 
-    public List<Movimentacao> findAll() {
-        return movimentacaoRepository.findAll();
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ContainerRepository containerRepository;
+
+    public boolean realizaMovimentacao(Long nrContainer, Endereco enderecoDestino) {
+        return true;
     }
 
 }
