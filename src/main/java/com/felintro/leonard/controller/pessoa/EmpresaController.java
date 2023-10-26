289a145bfa9d047b10e8b12fab784b1a8a2d6e06
Allan Felintro <allanpdu@gmail.com>
2023-10-25 21:23:15 -0300

    Implementação de método para editar o cadastro de empresas + melhora no método de carregamento do formulario de cadastro

--- src/main/java/com/felintro/leonard/controller/pessoa/EmpresaController.java
+++ src/main/java/com/felintro/leonard/controller/pessoa/EmpresaController.java
@@ -4,9 +4,11 @@ import com.felintro.leonard.dto.pessoa.EmpresaDTO;
 import com.felintro.leonard.service.pessoa.EmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,13 +24,25 @@ public class EmpresaController {
     private EmpresaService empresaService;
 
     @GetMapping("/formulario")
-    public String abrirFormularioCadastro() {
+    public String carregaPaginaFormulario(Long id, Model model) {
+        if(id != null) {
+            var empresaDTO = empresaService.buscaPorId(id);
+            model.addAttribute("empresaDTO", empresaDTO);
+        }
         return "cadastro/cadastro-empresa";
     }
 
     @PostMapping("/cadastrar")
+    @Transactional
     public String cadastrarEmpresa(EmpresaDTO empresaDTO) {
         empresaService.cadastrarEmpresa(empresaDTO);
+        return "redirect:/empresa/visualizar";
+    }
+
+    @PutMapping("/cadastrar")
+    @Transactional
+    public String alterarEmpresa(EmpresaDTO empresaDTO) {
+        empresaService.alterarEmpresa(empresaDTO);
         return "redirect:/empresa/visualizar";
     }
 
