1098dc806aab6859c3b3f39c938d24693236a75c
Allan Felintro <allanpdu@gmail.com>
2023-10-31 21:59:41 -0300

    Implementação de API para registrar pedidos

--- src/main/java/com/felintro/leonard/controller/pedido/PedidoCompraController.java
+++ src/main/java/com/felintro/leonard/controller/pedido/PedidoCompraController.java
@@ -1,18 +1,22 @@
 package com.felintro.leonard.controller.pedido;
 
 import com.felintro.leonard.dto.pedido.RegistraPedidoDTO;
-import com.felintro.leonard.dto.pedido.PedidoDTO;
+import com.felintro.leonard.dto.pessoa.EmpresaDTO;
 import com.felintro.leonard.service.pedido.PedidoService;
+import com.felintro.leonard.service.pessoa.EmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/pedido-compra")
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedido")
 public class PedidoCompraController {
 
     private static final String PEDIDO_REGISTRADO = "Pedido registrado com sucesso!";
@@ -20,6 +24,9 @@ public class PedidoCompraController {
 
     @Autowired
     private PedidoService pedidoService;
+
+    @Autowired
+    private EmpresaService empresaService;
 
     @PostMapping("/registrar")
     public ResponseEntity registrarPedidoCompra(@RequestBody RegistraPedidoDTO registraPedidoDTO) {
@@ -31,6 +38,11 @@ public class PedidoCompraController {
     public ResponseEntity cancelarPedidoCompra(@RequestBody Long nrPedido) {
         pedidoService.cancelarPedido(nrPedido);
         return new ResponseEntity(PEDIDO_CANCELADO, HttpStatus.OK);
+    }
+
+    @PostMapping("/teste")
+    public ResponseEntity testeEmpresa(@RequestBody EmpresaDTO empresaDTO) {
+        return new ResponseEntity(empresaDTO, HttpStatus.OK);
     }
 
 }
