94afa6c654e8c21503f53bd36455e87de473f861
Allan Felintro <allanpdu@gmail.com>
2023-11-02 14:55:51 -0300

    Implementação de Pack, para representar uma embalagem de produto único.

---
+++ src/main/java/com/felintro/leonard/model/estoque/Pack.java
@@ -0,0 +1,45 @@
+package com.felintro.leonard.model.estoque;
+
+import com.felintro.leonard.dto.estoque.PackDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author allan
+ **/
+
+@Entity
+@Table(name = "pack")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pack {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "nr_pack")
+    private Long nrPack;
+
+    @OneToOne
+    @JoinColumn(name = "id_produto", nullable = false)
+    private Produto produto;
+
+    @Column(name = "quantidade", nullable = false)
+    private int quantidade;
+
+    public PackDTO toDTO() {
+        return new PackDTO(this.nrPack, this.produto.toDTO(), this.quantidade);
+    }
+
+}
