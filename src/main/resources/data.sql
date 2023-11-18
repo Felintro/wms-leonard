72f130a37cbb0eb671ebf39cba6b1bd2b1e9d943
Allan Felintro <allanpdu@gmail.com>
2023-11-17 22:39:04 -0300

    Correção no script de atualização do banco de dados.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO empresa (email, nr_cnpj, nr_telefone, razao_social) VALUES ('teste@t
 INSERT INTO empresa (email, nr_cnpj, nr_telefone, razao_social) VALUES ('felintro@gmail.com', '55444333222211', '4433445566', 'Felintro S.A.');
 INSERT INTO empresa (email, nr_cnpj, nr_telefone, razao_social) VALUES ('tarara@email.com', '66777888999911', '44977885522', 'Tarara Ltda.');
 
-INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Leite Líder 1L Integral', 12, '17981001123456', '7981001123456');
+INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Leite Líder 1L Integral', 12, '17891001123456', '7891001123456');
 INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Arroz Abe 5kg', 6, '17891231456789', '7891231456789');
 INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Iogurte Vida Ativa Zero', 20, '17894564789456', '7894564789456');
 
