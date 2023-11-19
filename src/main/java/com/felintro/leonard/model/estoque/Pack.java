5349837596220b795396db1e56738c5cc091fc03
Allan Felintro <allanpdu@gmail.com>
2023-11-19 14:35:54 -0300

    Limpeza de código

--- src/main/java/com/felintro/leonard/model/estoque/Pack.java
+++ src/main/java/com/felintro/leonard/model/estoque/Pack.java
@@ -3,8 +3,6 @@ package com.felintro.leonard.model.estoque;
 import com.felintro.leonard.dto.estoque.PackDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
