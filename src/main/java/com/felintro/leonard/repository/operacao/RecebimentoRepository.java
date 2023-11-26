3b51c9e519e026d6e5a37ec37cb098c530a85c68
Allan Felintro <allanpdu@gmail.com>
2023-11-25 21:45:11 -0300

    Limpeza de código

--- src/main/java/com/felintro/leonard/repository/operacao/RecebimentoRepository.java
+++ src/main/java/com/felintro/leonard/repository/operacao/RecebimentoRepository.java
@@ -2,13 +2,14 @@ package com.felintro.leonard.repository.operacao;
 
 import com.felintro.leonard.model.operacao.Recebimento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * @author allan
  **/
-
+@Repository
 public interface RecebimentoRepository extends JpaRepository<Recebimento, Long> {
 
     Optional<Recebimento> findRecebimentoByPedidoNrPedido(Long nrPedido);
