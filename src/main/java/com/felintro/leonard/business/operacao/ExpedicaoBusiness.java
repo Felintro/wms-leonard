55a7e1ba2eb6f7cfae2da990e0686010c53b3d91
Allan Felintro <allanpdu@gmail.com>
2023-11-28 23:30:36 -0300

    Implementação inicial de estrutura para Expedição

--- src/main/java/com/felintro/leonard/business/operacao/ExpedicaoBusiness.java
+++ src/main/java/com/felintro/leonard/business/operacao/ExpedicaoBusiness.java
@@ -1,5 +1,10 @@
 package com.felintro.leonard.business.operacao;
 
+import com.felintro.leonard.repository.estoque.ContainerRepository;
+import com.felintro.leonard.repository.operacao.ExpedicaoRepository;
+import com.felintro.leonard.repository.operacao.SeparacaoRepository;
+import com.felintro.leonard.repository.pessoa.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,5 +13,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ExpedicaoBusiness {
+
+    @Autowired
+    private ExpedicaoRepository expedicaoRepository;
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    @Autowired
+    private ContainerRepository containerRepository;
+
+    @Autowired
+    private SeparacaoRepository separacaoRepository;
 
 }
--- src/main/java/com/felintro/leonard/controller/operacao/ExpedicaoController.java
+++ src/main/java/com/felintro/leonard/controller/operacao/ExpedicaoController.java
@@ -1,7 +1,19 @@
 package com.felintro.leonard.controller.operacao;
 
+import com.felintro.leonard.business.operacao.ExpedicaoBusiness;
+import com.felintro.leonard.dto.pedido.PedidoDTO;
+import com.felintro.leonard.enums.StatusPedido;
+import com.felintro.leonard.enums.TipoPedido;
+import com.felintro.leonard.service.pedido.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author allan
@@ -10,5 +22,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/expedicao")
 public class ExpedicaoController {
+
+    private static final String TELA_EXPEDICAO = "operacao/expedicao/expedicao";
+    private static final String TELA_INICIAL = "operacao/expedicao/selecao-cliente";
+    private static final String REDIRECT_FORMULARIO = "redirect:/expedicao/formulario";
+    private static final String REDIRECT_TELA_INICIAL = "redirect:/expedicao/selecao-pedido";
+
+    @Autowired
+    private ExpedicaoBusiness expedicaoBusiness;
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @GetMapping("/selecao-cliente")
+    public String carregaPaginaInicial(Model model) {
+        List<PedidoDTO> listaPedidosConcluidosDTO = pedidoService.buscarPorStatusETipo(StatusPedido.CONCLUIDO, TipoPedido.VENDA);
+        model.addAttribute("listaPedidosConcluidosDTO", listaPedidosConcluidosDTO);
+        return TELA_INICIAL;
+    }
+
+    @PostMapping("/expedir")
+    @Transactional
+    public String expedirContainer() {
+        return "";
+    }
+
+    @PostMapping("/finalizar")
+    @Transactional
+    public String finalizarExpedicao() {
+        return "";
+    }
+
 
 }
