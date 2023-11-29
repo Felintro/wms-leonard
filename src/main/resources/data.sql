8edfe2b6dc692764f99bc3590ffaaa3a25a1bfd9
Allan Felintro <allanpdu@gmail.com>
2023-11-28 23:35:06 -0300

    Ajustes nos nomes dos produtos.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ INSERT INTO empresa (email, nr_cnpj, nr_telefone, razao_social) VALUES ('teste@t
 INSERT INTO empresa (email, nr_cnpj, nr_telefone, razao_social) VALUES ('felintro@gmail.com', '55444333222211', '4433445566', 'Felintro S.A.');
 INSERT INTO empresa (email, nr_cnpj, nr_telefone, razao_social) VALUES ('tarara@email.com', '66777888999911', '44977885522', 'Tarara Ltda.');
 
-INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Leite Líder 1L Integral', 12, '17891001123456', '7891001123456');
-INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Arroz Abe 5kg', 6, '17891231456789', '7891231456789');
-INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Iogurte Vida Ativa Zero', 20, '17894564789456', '7894564789456');
+INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Leite 1L Integral', 12, '17891001123456', '7891001123456');
+INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Arroz Parboilizado 5Kg', 6, '17891231456789', '7891231456789');
+INSERT INTO produto (ds_descricao, fator_caixa, nr_dun14, nr_ean13) VALUES ('Iogurte Zero Lactose 1L', 20, '17894564789456', '7894564789456');
 
 INSERT INTO endereco (nr_rua, nr_predio, nr_apartamento) VALUES (1, 1, 1);
 INSERT INTO endereco (nr_rua, nr_predio, nr_apartamento) VALUES (1, 1, 2);
